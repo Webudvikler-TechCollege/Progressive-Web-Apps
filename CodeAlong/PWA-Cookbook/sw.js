15a346c037b1d4db1177defa2b03a0e0faf67e6f
Heinz K <heka@techcollege.dk>
2023-04-19 09:09:48 +0200

    SW Updates - comments og dynamisk cache

--- CodeAlong/PWA-Cookbook/sw.js
+++ CodeAlong/PWA-Cookbook/sw.js
@@ -1,5 +1,9 @@
+// Navn på App Shell Cache til assets filer 
 const staticCacheName = 'site-static-v3'
+// Navn på dynamisk cache
+const dynamicCacheName = 'site-dynamic-v1'
 
+// Array med assets filer til statisk cache
 const assets = [
 	'/',
 	'./index.html',
@@ -13,33 +17,69 @@ const assets = [
 	'https://fonts.gstatic.com/s/materialicons/v140/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
 ]
 
+// Install event
 self.addEventListener('install', event => {
-	//console.log('serviceworker has been installed...');
+	// Vent til alle opgaver er udført
 	event.waitUntil(
+		
+		/* Tilføj assets filer til statisk cache */
+
+		// Åbn statisk cache
 		caches.open(staticCacheName).then(cache => {
-			//console.log('Caching all assets')
+			// Tilføj array af assets filer til cache
 			cache.addAll(assets)
 		})	
 	)
 })
 
+// Activate event
 self.addEventListener('activate', event => {
-	console.log('serviceworker has been activated...')
+	// Vent til alle opgaver er udført
 	event.waitUntil(
+		
+		/* Slet tidligere cache versioner */
+		
+		// Kald alle cache nøgler (Navn på cache samlinger)
 		caches.keys().then(keys => {
+			// Returnerer et array af promises (et promis for hver fil)
 			return Promise.all(keys
+				// Filtrer alle som ikke er medlem af den nuværende cache version
 				.filter(key => key !== staticCacheName)
+				// Map filter array og slet filer
 				.map(key => caches.delete(key)))
 		})
 	)
 	return;
 })
 
+// Fetch event
 self.addEventListener('fetch', event => {
-	//console.log(event.request);
+	// Fix af problem med dynamisk cache og chrome-extension bug
+	if(!(event.request.url.indexOf('http') === 0)) return;
+
+	// Kontroller svar på request
 	event.respondWith(
+
+		/* Håndtering af cache match og dynamisk cache */
+
+		// Kig efter file match i cache
 		caches.match(event.request).then(cacheRes => {
-			return cacheRes || fetch(event.request)
+
+			// Returner hvis match fra cache - ellers hent fil på server
+			return cacheRes || fetch(event.request).then(fetchRes => {
+
+				// Åbn dynamisk cache
+				return caches.open(dynamicCacheName).then(cache => {
+
+					// Tilføj side til dynamisk cache
+					cache.put(event.request.url, fetchRes.clone())
+
+					// Returner request
+					return fetchRes
+				})
+			})
+
+
 		})
 	)
 })
